23da75c98de8203ebb7ac80395c92bf34fb9c171
jasonliprivate <77710014+jasonliprivate@users.noreply.github.com>
2021-03-11 17:32:12 -0800

    config

---
+++ redvre.882.%d.jpg/keyshot-configurator-imagemapping.js
@@ -0,0 +1,123 @@
+imagemappingdata =
+[
+    {
+        "image": "redvre.882.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+248",
+            "68+294"
+        ]
+    },
+    {
+        "image": "redvre.882.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+248",
+            "68+296"
+        ]
+    },
+    {
+        "image": "redvre.882.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+248",
+            "68+306"
+        ]
+    },
+    {
+        "image": "redvre.882.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+257",
+            "68+294"
+        ]
+    },
+    {
+        "image": "redvre.882.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+257",
+            "68+296"
+        ]
+    },
+    {
+        "image": "redvre.882.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+257",
+            "68+306"
+        ]
+    },
+    {
+        "image": "redvre.882.7.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+269",
+            "68+294"
+        ]
+    },
+    {
+        "image": "redvre.882.8.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+269",
+            "68+296"
+        ]
+    },
+    {
+        "image": "redvre.882.9.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+269",
+            "68+306"
+        ]
+    },
+    {
+        "image": "redvre.882.10.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+357",
+            "68+294"
+        ]
+    },
+    {
+        "image": "redvre.882.11.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+357",
+            "68+296"
+        ]
+    },
+    {
+        "image": "redvre.882.12.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "46+357",
+            "68+306"
+        ]
+    }
+];
---
+++ redvre.882.%d.jpg/keyshot-configurator-modeldata.js
@@ -0,0 +1,78 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "material.png",
+                    "multimaterials": [
+                        "46+248"
+                    ],
+                    "name": "Material"
+                },
+                {
+                    "icon": "material%20%231.png",
+                    "multimaterials": [
+                        "46+257"
+                    ],
+                    "name": "Material #1"
+                },
+                {
+                    "icon": "material%20%232.png",
+                    "multimaterials": [
+                        "46+269"
+                    ],
+                    "name": "Material #2"
+                },
+                {
+                    "checked": true,
+                    "icon": "material%20%233.png",
+                    "multimaterials": [
+                        "46+357"
+                    ],
+                    "name": "Material #3"
+                }
+            ],
+            "name": "Leather White 1000mm #3"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "aluminum%20polished%20%231.png",
+                    "multimaterials": [
+                        "68+294"
+                    ],
+                    "name": "Aluminum Polished #1"
+                },
+                {
+                    "icon": "material-2.png",
+                    "multimaterials": [
+                        "68+296"
+                    ],
+                    "name": "Material"
+                },
+                {
+                    "icon": "material%20%231-2.png",
+                    "multimaterials": [
+                        "68+306"
+                    ],
+                    "name": "Material #1"
+                }
+            ],
+            "name": "Aluminum Polished #1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "materialgroups": [
+                "Leather White 1000mm #3",
+                "Aluminum Polished #1"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
